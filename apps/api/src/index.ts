06c4e07640e78d3f34ff7e574b82ce7fde435163
MiguelBizzi <migueloliveirabizzi@gmail.com>
2024-05-19 12:37:31 -0300

    feat: adicionando casl

---
+++ apps/api/src/index.ts
@@ -0,0 +1,8 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteUser = ability.can('delete', 'User')
+
+const userCannotDeleteUser = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse, userCanDeleteUser, userCannotDeleteUser)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,1 +1,27 @@
-const teste = 'oi'
+import {
+  createMongoAbility,
+  ForcedSubject,
+  CreateAbility,
+  MongoAbility,
+  AbilityBuilder,
+} from '@casl/ability'
+
+const actions = ['manage', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { can, cannot, build } = new AbilityBuilder(createAppAbility)
+
+can('invite', 'User')
+
+export const ability = build()
